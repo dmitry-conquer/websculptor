fad824263b7edaeae87291d476aeffc3aba282c9
Dmytro Frolov <dmitry.conquer@gmail.com>
2023-08-04 15:51:13 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+// import { consoleInfo, isWebp() } from './config/functions.js';
+// import { hasErrors } from './modules/forms.js';
+// import { initSpoiler, initDdropdown } from './modules/spoilers.js';
+// import { initTabs } from './modules/tabs.js';
+// import { initSliders } from './modules/sliders.js';
+// import { initHeader } from './modules/header.js';
+// import { initModals, openModalIf } from './modules/modals.js';
+// import { useDynamicAdapt } from './modules/dynamicAdapt.js';
+// import gsap from "gsap";
+// import { ScrollTrigger } from 'gsap/ScrollTrigger.js';
+// import AOS from 'aos';
+// import Rellax from "rellax";
+// import fslightbox from 'fslightbox';
+// import Ellipsis from 'ellipsis.js';
+// import Choices from 'choices.js';
+// import { CountUp } from '../../node_modules/countup.js/dist/countUp.js';
+// import noUiSlider from 'nouislider';
+// import MicroModal from 'micromodal';
+
+// > - - - - - - - - [app development] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function app() {
+   // - - - - - - - [app {START}] - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   // - - - - - - - [app {END}] - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
+
+document.addEventListener('DOMContentLoaded', app);
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ["./src/scss/**/*.scss", "./src/**/*.html"],
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
